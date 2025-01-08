f48d5f20b9f09defd48935353830e2657c7e8a82
abderrahmane140 <absarabdo4@gmail.com>
2025-01-09 00:43:24 +0100

    connect the database

--- OrderService/app.js
+++ OrderService/app.js
@@ -1,11 +1,14 @@
 require('dotenv').config()
 const express = require('express');
+const mongoose = require('mongoose')
 
 
 const app = express();
 
 app.use(express.json())
 
-app.listen(process.env.PORT,()=>{
-    console.log("the app listen on the port",process.env.PORT); 
+mongoose.connect(process.env.MONGO_URL).then(() => {
+    app.listen(process.env.PORT,()=>{
+        console.log("conncet to db & listen to port",process.env.PORT); 
+    })
 })
