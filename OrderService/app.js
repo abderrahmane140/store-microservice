76c709d456cc423b463bac8ea9416ba3a4cd3bc5
abderrahmane140 <absarabdo4@gmail.com>
2025-01-25 22:15:04 +0100

    integrate the routes on the app file

--- OrderService/app.js
+++ OrderService/app.js
@@ -2,10 +2,13 @@ require('dotenv').config()
 const express = require('express');
 const mongoose = require('mongoose')
 
-
+const orderRouter = require('./routes/orderRoute')
 const app = express();
 
 app.use(express.json())
+
+//routes
+app.use('/api/order', orderRouter)
 
 mongoose.connect(process.env.MONGO_URL).then(() => {
     app.listen(process.env.PORT,()=>{
