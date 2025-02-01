bede86939d380c93cac38a1ba5e892854c8d84fe
abderrahmane140 <absarabdo4@gmail.com>
2025-02-01 21:09:53 +0100

    Integrate order routes and apply authentication middleware in app.js

--- Api_Gateway/app.js
+++ Api_Gateway/app.js
@@ -1,11 +1,19 @@
 require('dotenv').config();
 const express = require('express');
+const logger = require('./middlewares/logger');
+const authenticate = require('./middlewares/authenticate')
 
 const app = express();
 
 app.use(express.json());
 
+app.use(logger);
+
+app.use('/api/', authenticate, require('./routes/Product'));
+app.use('/api/',authenticate, require('./routes/Order'));
+
+
 
 app.listen(process.env.PORT, () => {
-    console.log("listen to port", process.env.PORT);
-})
+    console.log("API Gateway running on port", process.env.PORT);
+});
