71e912cdf2e90aee9b9a436f17b99487e6421c2c
abderrahmane140 <absarabdo4@gmail.com>
2025-02-08 02:02:22 +0100

    Added React Router and Setup Page Navigation

--- Client/src/App.jsx
+++ Client/src/App.jsx
@@ -1,17 +1,40 @@
 import Footer from "./components/footer";
 import Header from "./components/header";
+import {BrowserRouter, Routes, Route} from 'react-router-dom'
+import Home from "./pages/Home";
+import Products from "./pages/Products";
+import Orders from "./pages/Orders";
+import ProductsDetails from "./pages/ProductsDetails";
+import SignIn from "./pages/SignIn";
+import SignUp from "./pages/SignUp";
 
 function App() {
   return (
     <div className="flex flex-col min-h-screen bg-slate-950">
-      <Header />
-      
-      {/* Main Content - Fills available space */}
-      <main className="flex-1 flex items-center justify-center text-white">
-        hello world
-      </main>
+      <BrowserRouter>
+        <Header />
+        
+        {/* Main Content - Fills available space */}
+        <main className="flex-1 flex items-center justify-center text-white">
+          <Routes>
+            <Route path="/" element={<Home/>}/>
 
-      <Footer />
+            <Route path="/products" element={<Products/>}/>
+
+            <Route path="/orders" element={<Orders/>}/>
+
+            <Route path="/product/:id" element={<ProductsDetails/>}/>
+
+            <Route path="/signIn" element={<SignIn/>}/>
+
+            <Route path="/signup" element={<SignUp/>}/>
+
+
+          </Routes>
+        </main>
+
+        <Footer />
+      </BrowserRouter>
     </div>
   );
 }
--- Client/src/components/Header.jsx
+++ Client/src/components/Header.jsx
@@ -1,48 +1,31 @@
 import React from "react";
+import { Link } from "react-router-dom";
 import { FaArrowRight } from "react-icons/fa";
 
 export default function Header() {
   return (
-    // <nav className="flex justify-between items-center px-6 py-4 text-white  ">
-    //   {/* Logo & Navigation Links */}
-    //   <div className="flex items-center gap-8">
-    //     <p className="font-bold text-xl uppercase">Store</p>
-
-    //     <div className="flex gap-6">
-    //       <a href="#" className="p-2 rounded-md transition font-semibold hover:bg-gray-900 bg-transparent">Product</a>
-    //       <a href="#" className="p-2 rounded-md transition font-semibold hover:bg-gray-900 bg-transparent">Order</a>
-    //     </div> 
-    //   </div>
-
-    //   {/* Auth Buttons */}
-    //   <div className="flex gap-4">
-    //     <button className="px-4 py-2 transition font-semibold hover:bg-gray-900 bg-transparen rounded-md">
-    //       Sign In
-    //     </button>
-    //     <button className="flex items-center gap-1 bg-white px-3 py-2 text-black rounded hover:bg-gray-200 transition" >
-    //       Sign Up
-    //       <FaArrowRight />
-    //     </button>
-    //   </div>
-    // </nav>
     <nav className="flex justify-between items-center px-6 py-4 text-white relative">
     {/* Logo & Navigation */}
     <div className="flex items-center gap-10">
       <p className="text-white font-bold text-2xl">Store</p>
       <div className="flex gap-8">
-        <a href="#" className="p-2 rounded-md transition font-semibold hover:bg-gray-900 bg-transparent">Product</a>
-        <a href="#" className="p-2 rounded-md transition font-semibold hover:bg-gray-900 bg-transparent">Order</a>
+        <Link to="/products" className="p-2 rounded-md transition font-semibold hover:bg-gray-900 bg-transparent">Product</Link>
+        <Link to="/orders" className="p-2 rounded-md transition font-semibold hover:bg-gray-900 bg-transparent">Order</Link>
       </div>
     </div>
   
     {/* Auth Buttons */}
     <div className="flex gap-4">
          <button className="px-4 py-2 transition font-semibold hover:bg-gray-900 bg-transparen rounded-md cursor-pointer">
-           Sign In
+          <Link to="/signIn">
+            Sign In
+          </Link>
          </button>
-      <button className="flex items-center gap-1 bg-white px-3 py-2 text-black rounded hover:bg-gray-200 transition cursor-pointer" >
-        Sign Up
-        <FaArrowRight />
+      <button className=" bg-white px-3 py-2 text-black rounded hover:bg-gray-200 transition cursor-pointer" >
+        <Link to="/signup" className="flex gap-2 items-center">
+          Sign Up
+          <FaArrowRight />
+        </Link>
       </button>
     </div>
   
